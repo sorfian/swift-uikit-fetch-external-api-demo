44023828f4d22aa1651d80b7fd9c154e96040e68
Sorfian <sorfian.tkj@gmail.com>
2023-03-25 20:57:29 +0800

    Initial Commit

---
+++ Fake Loans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fake Loans
+//
+//  Created by Sorfian on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
